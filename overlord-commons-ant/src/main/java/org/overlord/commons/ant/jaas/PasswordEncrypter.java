792df2927a0ca40664dcf7a07d34a7eea6bd4f1e
dvirgiln <david.virgil.naranjo@gmail.com>
2014-07-31 14:19:27 +0200

    SRAMP-380 Passwords in clear text when running in Fuse 6.1

--- overlord-commons-ant/src/main/java/org/overlord/commons/ant/jaas/PasswordEncrypter.java
+++ overlord-commons-ant/src/main/java/org/overlord/commons/ant/jaas/PasswordEncrypter.java
@@ -15,19 +15,17 @@
  */
 package org.overlord.commons.ant.jaas;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 /**
  * Ant task that generates an password with md5 encryption and hexadecimal
  * encoding, using exactly the same code used in apache karaf.
- * 
+ *
  * @author David Virgil Naranjo
  */
-public class MD5PasswordEncrypter extends Task {
+public class PasswordEncrypter extends Task {
 
     private String password;
 
@@ -87,47 +85,13 @@ public class MD5PasswordEncrypter extends Task {
         if (password == null || password.equals("")) { //$NON-NLS-1$
             throw new BuildException("\tThe password property is required for this task."); //$NON-NLS-1$
         }
-        try {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            md.update(password.getBytes());
-            //Get the hash's bytes
-            byte[] bytes = md.digest();
-
-            //Get complete hashed password in hex format
-            generatedPassword = new String(hexEncode(bytes));
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            throw new BuildException("\tThere is a problem encrypting the password with MD5 algorithm"); //$NON-NLS-1$
-        }
+        // Get complete hashed password in hex format
+        generatedPassword = DigestUtils.sha256Hex(password);
 
         if (addproperty != null && !addproperty.equals("")) { //$NON-NLS-1$
             getProject().setProperty(addproperty, generatedPassword);
         }
     }
 
-    private static final byte[] hexTable = { (byte) '0', (byte) '1', (byte) '2', (byte) '3', (byte) '4', (byte) '5', (byte) '6', (byte) '7',
-            (byte) '8', (byte) '9', (byte) 'a', (byte) 'b', (byte) 'c', (byte) 'd', (byte) 'e', (byte) 'f' };
-
-    /**
-     * Hex encode.
-     *
-     * @param in
-     *            the in
-     * @return the string
-     */
-    public static String hexEncode(byte[] in) {
-        int inOff = 0;
-        int length = in.length;
-        byte[] out = new byte[length * 2];
-        for (int i = 0, j = 0; i < length; i++, j += 2) {
-            out[j] = hexTable[(in[inOff] >> 4) & 0x0f];
-            out[j + 1] = hexTable[in[inOff] & 0x0f];
-            inOff++;
-    }
-        return new String(out);
-    }
 
 }
