3cc706e3dabf73b734d4b40f2ae5a026eb571b4e
dvirgiln <david.virgil.naranjo@gmail.com>
2014-09-12 12:42:30 +0200

    OVERLORD-130 GenerateSamlKeystoreUtil currently using three deprecated BC classes

--- overlord-commons-dist/overlord-commons-karaf-commands/src/main/java/org/overlord/commons/karaf/commands/saml/GenerateSamlKeystoreUtil.java
+++ overlord-commons-dist/overlord-commons-karaf-commands/src/main/java/org/overlord/commons/karaf/commands/saml/GenerateSamlKeystoreUtil.java
@@ -15,25 +15,31 @@
  */
 package org.overlord.commons.karaf.commands.saml;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigInteger;
+import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import javax.security.auth.x500.X500Principal;
-
-import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
-import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
 import org.overlord.commons.karaf.commands.i18n.Messages;
 
 /**
@@ -169,25 +175,15 @@ public class GenerateSamlKeystoreUtil {
        calendar.add(Calendar.DAY_OF_YEAR, validity);
         // time from which certificate is valid
         Date expiryDate = calendar.getTime();            // time after which certificate is not valid
-        BigInteger serialNumber =new BigInteger("10");       // serial number for certificate //$NON-NLS-1$
-                 // private key of the certifying authority (ca) certificate
 
-        X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
-        X500Principal              subjectName = new X500Principal(dname);
 
-        certGen.setSerialNumber(serialNumber);
-        certGen.setIssuerDN(subjectName);
-        certGen.setNotBefore(date);
-        certGen.setNotAfter(expiryDate);
-        certGen.setSubjectDN(subjectName);
-        certGen.setPublicKey(keypair.getPublic());
-        certGen.setSignatureAlgorithm("SHA256withRSA"); //$NON-NLS-1$
+        X509v3CertificateBuilder certBuilder = new X509v3CertificateBuilder(new X500Name(dname), new BigInteger("1"), date, expiryDate, new X500Name(
+                dname), SubjectPublicKeyInfo.getInstance(keypair.getPublic().getEncoded()));
+        byte[] certBytes = certBuilder.build(new JCESigner(privKey, "SHA256withRSA")).getEncoded();
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(certBytes));
 
-        certGen.addExtension(X509Extensions.SubjectKeyIdentifier, false,
- new SubjectKeyIdentifierStructure(keypair.getPublic()));
-
-        X509Certificate cert = certGen.generate(keypair.getPrivate(), providerName);
-        chain[0] = cert;
+        chain[0] = certificate;
 
         keyStore.setKeyEntry(alias, privKey, keyPass, chain);
     }
@@ -227,4 +223,50 @@ public class GenerateSamlKeystoreUtil {
         return c.getTime();
     }
 
+    private static class JCESigner implements ContentSigner {
+
+        private static final AlgorithmIdentifier PKCS1_SHA256_WITH_RSA_OID = new AlgorithmIdentifier(
+                new ASN1ObjectIdentifier("1.2.840.113549.1.1.11"));
+
+        private Signature signature;
+        private ByteArrayOutputStream outputStream;
+
+        public JCESigner(PrivateKey privateKey, String signatureAlgorithm) {
+            if (!"SHA256withRSA".equals(signatureAlgorithm)) {
+                throw new IllegalArgumentException("Signature algorithm \"" + signatureAlgorithm + "\" not yet supported");
+            }
+            try {
+                this.outputStream = new ByteArrayOutputStream();
+                this.signature = Signature.getInstance(signatureAlgorithm);
+                this.signature.initSign(privateKey);
+            } catch (GeneralSecurityException gse) {
+                throw new IllegalArgumentException(gse.getMessage());
+            }
+        }
+
+        @Override
+        public AlgorithmIdentifier getAlgorithmIdentifier() {
+            if (signature.getAlgorithm().equals("SHA256withRSA")) {
+                return PKCS1_SHA256_WITH_RSA_OID;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public OutputStream getOutputStream() {
+            return outputStream;
+        }
+
+        @Override
+        public byte[] getSignature() {
+            try {
+                signature.update(outputStream.toByteArray());
+                return signature.sign();
+            } catch (GeneralSecurityException gse) {
+                gse.printStackTrace();
+                return null;
+            }
+        }
+    }
 }
